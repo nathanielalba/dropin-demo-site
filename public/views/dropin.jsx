83944d54284033a08e9a7f2b4df15dacf0ea58b1
chgrp-hung <hung@thechgrp.com>
2016-10-26 20:27:03 -0700

    tried to update dealercode and make dropin auto url configurable.

--- public/views/dropin.jsx
+++ public/views/dropin.jsx
@@ -8,7 +8,8 @@ var DropIn = React.createClass({
 
     console.log('Live video has started');
     console.log('Dealer code is', DealerCode);
-    window.location.href = `https://autoqa.dropinauto.com/dealer/${DealerCode}?vin=${this.props.vin}&refererUrl=${window.location.href}`;
+    window.location.href = `${DropInAutoUrl}/${DealerCode}?vin=${this.props.vin}&refererUrl=${window.location.href}`;
+    console.log(window.location.href);
   },
   render: function() {
     return (
